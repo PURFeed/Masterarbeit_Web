8027685cfc36521408c01851d20837e0222e92bf
PURFeed <phillipp.gohlke@googlemail.com>
2024-09-12 22:13:20 +0200

    Testen der Datenbank

--- oberflaeche/views.py
+++ oberflaeche/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Techniques
+#from .models import Techniques
 from mitreattack.stix20 import MitreAttackData
 
 # Create your views here.
@@ -8,15 +8,15 @@ def home(request):
     mitre_attack_data = MitreAttackData("enterprise-attack.json")
     alle_technik = mitre_attack_data.get_techniques(remove_revoked_deprecated=True)
 
-    for s in alle_technik:
-        technique = Techniques(
-            name=alle_technik[s].name,
-            id=alle_technik[s].id,
-            type=alle_technik[s].type,
-            description=alle_technik[s].description,
-            external_id=alle_technik[s].external_reference
-        )
-        technique.save()
+   # for s in alle_technik:
+    #    technique = Techniques(
+    #       name=alle_technik[s].name,
+    #       id=alle_technik[s].id,
+    #       type=alle_technik[s].type,
+    #       description=alle_technik[s].description,
+    #       external_id=alle_technik[s].external_reference
+    #   )
+    #   technique.save()
     return render(request, 'suche.html')
 
 def index(request):
