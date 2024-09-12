bdd1264348b7b55eca241584aaaeadd4df06c242
PURFeed <phillipp.gohlke@googlemail.com>
2024-09-12 21:03:19 +0200

    Anfang der html-Seiten

--- Masterarbeit_Mitre_Attack/urls.py
+++ Masterarbeit_Mitre_Attack/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from oberflaeche import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
 ]
