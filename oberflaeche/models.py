ed20e929c7938dda4b94f6225d0314fd4ab722b2
PURFeed <phillipp.gohlke@googlemail.com>
2024-09-12 18:51:33 +0200

    Start mit DB

--- oberflaeche/models.py
+++ oberflaeche/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Techniques(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100, )
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    external_reference = models.TextField(max_length=1000)
+
+class Mitigations(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    external_reference = models.TextField(max_length=1000)
+
+class Software(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    external_reference = models.TextField(max_length=1000)
+
+class Groups(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    external_reference = models.TextField(max_length=1000)
+
+class Taktiks(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    external_reference = models.TextField(max_length=1000)
